68e76fadae0faab3648f93f97b1c676735fef274
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-10-13 09:29:28 -0500

    Custom User Model adjustments

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
-# Register your models here.
-admin.site.register(Account)
+# To avoid password to been seen in admin panel
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined',
+                    'is_active',)
+    
+    list_display_links = ('email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('date_joined',)
+    
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = () # To make password read-only
+
+admin.site.register(Account, AccountAdmin)
