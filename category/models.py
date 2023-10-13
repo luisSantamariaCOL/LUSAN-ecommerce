b70819e9ef2cb043afa6b71d06d1bdd1a3b70a2d
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-10-13 10:11:37 -0500

    category slug pre-populated

--- category/models.py
+++ category/models.py
@@ -3,7 +3,15 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(max_length=100, unique=True) # url of the category
+
+    """
+    A "slug" is a way of generating a valid URL, generally using data already obtained.
+
+    An example:
+    <title> The 46 Year Old Virgin </title>
+    <slug> the-46-year-old-virgin </slug>
+    """
+    slug = models.SlugField(max_length=100, unique=True) # url of the category
     description = models.TextField(max_length=255, blank=True)
     image = models.ImageField(upload_to='photos/categories/', blank=True)
 
