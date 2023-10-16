bb1d7c6121e8c4ef6042e802a231227f410d74ef
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-10-16 08:36:06 -0500

    products displayed by category

--- store/views.py
+++ store/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from category.models import Category
 
-def store(request):
+def store(request, category_slug=None):
+    categories = None
+    products = None
 
-    products = Product.objects.all().filter(is_available=True)
+    if category_slug:
+        print("hola mundo")
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+    
+    else:
+        products = Product.objects.all().filter(is_available=True)
+
     product_count = products.count()
 
     context = {
