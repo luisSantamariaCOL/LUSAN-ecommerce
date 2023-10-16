6ef1dd778d716afa4193065fd4cfc9699e222a32
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-10-15 20:19:10 -0500

    products displayed in Homepage

--- lusan/views.py
+++ lusan/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import request, HttpResponse
-
+from store.models import Product
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'home.html', context)
 
  
