d8400ed170d51aae6e4c44de839caeadb251d490
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-10-20 13:29:32 -0500

    remove and add item from cart functionality

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
+from django.http import HttpResponse
 
 # Create your views here.
  
@@ -37,10 +38,45 @@ def add_cart(request, product_id):
             quantity = 1,
             cart = cart,
         )
+        
         cart_item.save()
 
-   
     return redirect('cart')
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
+def cart(request, total=0, quantity=0, cart_items=None):
+
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        
+        iva = 0.19 * total
+        grand_total = total + iva
+
+
+    except Cart.ObjectNotExist:
+        pass
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'iva': iva,
+        'grand_total' : grand_total,
+    }
+
+    return render(request, 'store/cart.html', context)
