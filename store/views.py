5b7158df089713f2516a8a6ef09c2b4cdb786c8f
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-10-22 08:39:59 -0500

    check for product added to cart functionality

--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.views import _cart_id
+from carts.models import CartItem
+from django.http import HttpResponse
+
 
 def store(request, category_slug=None):
     categories = None
@@ -24,14 +28,18 @@ def store(request, category_slug=None):
     return render(request, 'store/store.html', context)
 
 def product_detail(request, category_slug, product_slug):
+
     try:
         # single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        single_product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        
     except Exception as e:
         raise e
     
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
 
 
