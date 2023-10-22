aa3e824b9cb616a72ab9ef07e9c1d962c5f4337b
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-10-22 11:34:30 -0500

    products paged in store

--- store/views.py
+++ store/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
-from carts.views import _cart_id
 from carts.models import CartItem
 from django.http import HttpResponse
 
+from carts.views import _cart_id
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def store(request, category_slug=None):
     categories = None
     products = None
 
     if category_slug:
-        print("hola mundo")
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
     
     else:
         products = Product.objects.all().filter(is_available=True)
-
-    product_count = products.count()
+        paginator = Paginator(products, 6) # second argument represents the number of items you want to show
+        page = request.GET.get('page') # capture the number of the store page
+        paged_products = paginator.get_page(page) # stores the number of items of paginator in the paged_products variable
 
     context = {
-        'products': products,
-        'product_count': product_count,
+        'products': paged_products,
+        'product_count': products.count(),
     }
 
     return render(request, 'store/store.html', context)
