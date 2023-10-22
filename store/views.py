a68ca3e2b4c34e932576da7b2a7589177daeceaa
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-10-22 17:34:17 -0500

    Search functionality implemented

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from django.http import HttpResponse
+from django.db.models import Q
 
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
@@ -49,3 +50,21 @@ def product_detail(request, category_slug, product_slug):
 
 
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+
+    products = ""
+    product_count = 0
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword)) # We need to use Q for OR queries
+            product_count = products.count()
+    
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+
+    return render(request, 'store/store.html', context)
