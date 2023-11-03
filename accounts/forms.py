ee6035d46057e8f413765e7f84a5c4379e2222ca
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-11-03 12:58:55 -0500

    model form for registration implemented

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,8 +15,23 @@ class RegistrationForm(forms.ModelForm):
         - fields: A list of field names to include in the form from the Account model.
     """
 
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
+
+    
+
     class Meta:
         # Link the form to the Account model
         model = Account
         # Define the fields that will be included in the form
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
