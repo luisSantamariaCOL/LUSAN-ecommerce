f1734f404e6216e7809cc7db3a57b4131e712288
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-11-05 14:12:23 -0500

    Django Message Alerts added

--- accounts/temp.py
+++ accounts/temp.py
@@ -2,33 +2,28 @@ from django import forms
 from .models import Account
 
 class RegistrationForm(forms.ModelForm):
-    # Custom password fields with placeholders and bootstrap class
+    # Moved password fields to the top to improve readability
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter Password', 'class': 'form-control'}))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'form-control'}))
 
     class Meta:
-        model = Account # Specify the model for form generation
+        model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
-
-        # Custom widgets with placeholders and bootstrap class for model fields
         widgets = {
             'first_name': forms.TextInput(attrs={'placeholder': 'Enter First Name', 'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'placeholder': 'Enter Last Name', 'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'placeholder': 'Enter Email Address', 'class': 'form-control'}),
             'phone_number': forms.TextInput(attrs={'placeholder': 'Enter Phone Number', 'class': 'form-control'}),
+            # Password field does not need to be redefined here as it is already customized above
         }
 
-
     def clean(self):
-        # Custom clean method to compare password fields
-        cleaned_data = super().clean()
+        cleaned_data = super().clean()  # No need to specify the class name explicitly
         password = cleaned_data.get('password')
         confirm_password = cleaned_data.get('confirm_password')
 
-        # Check if both password entries match
         if password != confirm_password:
-            # Raise validation error if there is a mismatch
-            raise forms.ValidationError("Password does not match!")
+            self.add_error('confirm_password', "Passwords do not match!")
+            # It's better to use add_error for field-specific validation messages
 
-
-
+        return cleaned_data  # Always return the cleaned data at the end of the clean method
